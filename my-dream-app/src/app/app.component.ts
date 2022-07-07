5dfa69d13d5527a5d4aabc63b1345a31fdc0d8b0
Diego Pacori <dpacoria@unsa.edu.pe>
2022-07-07 00:02:45 -0500

    Se creo la funcion deleteUser la cual busca el nombre que coincida y con el splice lo borra, y en el html se creo un for que itere entre todos los usuarios y genere un boton el cual al hacer click llame a la funcion antes mencionada enviando el usuario

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -36,5 +36,13 @@ export class AppComponent {
   sayHello(){
     alert("Hola desde app.component");
   }
+
+  deleteUser(user: string){
+    for(let i=0; i<this.users.length; i++){
+      if(user==this.users[i]){
+        this.users.splice(i,1);
+      }
+    }
+  }
 }
 
