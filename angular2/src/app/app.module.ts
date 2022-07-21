c269ee014b784fd3f46245021ad0485ba7d12957
Diego Pacori <dpacoria@unsa.edu.pe>
2022-07-20 21:56:40 -0500

    Se agrego un video, con la terminal ejecutamos npm install @angular/youtube-player

--- angular2/src/app/app.module.ts
+++ angular2/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { YouTubePlayerModule } from '@angular/youtube-player';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    YouTubePlayerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
